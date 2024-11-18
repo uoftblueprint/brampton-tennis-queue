adf294c0b2322ba34935d0a46057f0dfa46c43dc
hamaguchi <93605959+LeandroHamaguchi@users.noreply.github.com>
2024-11-18 16:36:13 -0500

    Implemented advanceQueue call in endSession and input validation in utility

--- backend/utils/advanceQueue.js
+++ backend/utils/advanceQueue.js
@@ -2,12 +2,23 @@
 // const sendWebNotification = require('./sendWebNotification'); // Import sendWebNotification 
 
 async function advanceQueue(locationData) {
-    const { activeFirebaseUIDs, activeNicknames, activeStartTimes, queueFirebaseUIDs, queueNicknames, activeWaitingPlayers } = locationData;
+    const { activeFirebaseUIDs, activeNicknames, activeStartTimes, activeWaitingPlayers, queueFirebaseUIDs, queueNicknames } = locationData;
+
+    // If there are no players in the queue, return early (don't advance the queue)
+    if (queueFirebaseUIDs.length === 0) {
+        return;
+    }
+
+    // Get empty court index, and validate
+    const emptyCourtIdx = activeFirebaseUIDs.findIndex((firebaseUID) => firebaseUID.startsWith('Empty'));
+    // if there are no empty courts, emptyCourtIdx will be -1 (don't advance the queue)
+    if (emptyCourtIdx === -1) { 
+        return;
+    }
 
     // Move first player in the queue to active
     const firstPlayerUID = queueFirebaseUIDs.shift();
     const firstPlayerNickname = queueNicknames.shift();
-    const emptyCourtIdx = activeFirebaseUIDs.findIndex((firebaseUID) => firebaseUID.startsWith('Empty'));
     activeFirebaseUIDs[emptyCourtIdx] = firstPlayerUID; 
     activeNicknames[emptyCourtIdx] = firstPlayerNickname;
     activeStartTimes[emptyCourtIdx] = new Date();

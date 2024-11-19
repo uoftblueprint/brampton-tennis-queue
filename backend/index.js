ed11b667a13e5ce45073eb1677fbd64a28d344ba
Alan Su <alansu1010@gmail.com>
2024-11-19 16:43:20 -0500

    Merge with main

--- backend/index.js
+++ backend/index.js
@@ -21,14 +21,11 @@ const expectedWaitTimeRoute = require('./routes/expectedWaitTimeRoute');
 const addUnknownsRoute = require('./routes/addUnknownsRoute');
 const leaveQueueRoute = require('./routes/leaveQueueRoute');
 const endSessionRoute = require('./routes/endSessionRoute');
-<<<<<<< HEAD
 const dynamicBufferRoute = require('./routes/dynamicBufferRoute');
-=======
 const getTakenRoute = require('./routes/getTakenRoute');
 const currentStateRoute = require('./routes/currentStateRoute');
 const joinGameRoute = require('./routes/joinGameRoute');
 const advanceQueueRoute = require('./routes/advanceQueueRoute');
->>>>>>> origin/main
 
 // Use imported routes
 app.use('/api', resetCourtsRoute);  // resetCourts endpoint
@@ -36,14 +33,11 @@ app.use('/api', expectedWaitTimeRoute);  // expectedWaitTime endpoint
 app.use('/api', addUnknownsRoute);  // addUnknowns endpoint
 app.use('/api', leaveQueueRoute);  // leaveQueue endpoint
 app.use('/api', endSessionRoute);  // endSession endpoint
-<<<<<<< HEAD
 app.use('/api', dynamicBufferRoute);  // dynamicBuffer endpoint
-=======
 app.use('/api', getTakenRoute); // getTaken endpoint
 app.use('/api', currentStateRoute); // currentState endpoint
 app.use('/api', joinGameRoute); // DUMMY joinGame endpoint - REPLACE WITH ACTUAL CODE IN ROUTE FILE!
 app.use('/api', advanceQueueRoute);  // advanceQueue endpoint
->>>>>>> origin/main
 
 // Default route
 app.get('/', (req, res) => {

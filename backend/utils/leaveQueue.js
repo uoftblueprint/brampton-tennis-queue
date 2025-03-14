474c71b12f38f22960dec20cc7e3e5140ceb4e26
Steven Lin <yuchenglin0705@gmail.com>
2025-03-14 17:25:05 -0400

    Initial commit for Update /leaveQueue

--- backend/utils/leaveQueue.js
+++ backend/utils/leaveQueue.js
@@ -1,6 +1,15 @@
-async function leaveQueue(locationData, firebaseUID) {
-    const { queueFirebaseUIDs, queueNicknames, queueJoinTimes, queueTokens } = locationData;
+import dynamicBuffer from "./dynamicBuffer";
 
+async function leaveQueue(locationData, firebaseUID) {
+    const { 
+        queueFirebaseUIDs, 
+        queueNicknames, 
+        queueJoinTimes, 
+        queueTokens,
+        activeStartTimes,
+        activeWaitingPlayers,
+        activeNicknames} = locationData;
+    
     // Check whether the player exists
     const playerIndex = queueFirebaseUIDs.indexOf(firebaseUID);
     if (playerIndex === -1) {
@@ -12,6 +21,35 @@ async function leaveQueue(locationData, firebaseUID) {
     queueNicknames.splice(playerIndex, 1);
     queueJoinTimes.splice(playerIndex, 1);
     queueTokens.splice(playerIndex, 1);
+
+    // Merge activeStartTimes and activeWaitingPlayers as a tuple array
+    const activePlayers = activeStartTimes.map((time, index) => [time, index]);
+
+    // Remove all elements where the nickname contains a bracket
+    activePlayers.sort((a, b) => a[0] - b[0]);
+    for (let i = 0; i < activePlayers.length; i++) {
+        const [time, index] = activePlayers[i];
+        if (activeNicknames[index].search('[') != -1) {
+            activeNicknames.splice(i, 1);
+        }
+    }
+
+    // Assert to ensure the length of activePlayers matches the count of true values in activeWaitingPlayers
+    console.assert(activePlayers.length === activeWaitingPlayers.filter(Boolean).length, "Mismatch between activePlayers length and true count in activeWaitingPlayers");
+
+    let remainingQueueLength = queueNicknames.length;
+
+    if (remainingQueueLength == activePlayers.length) {
+        return 200;
+    } else {
+        const [time, index] = activePlayers[activePlayers.length - 1];
+        activeWaitingPlayers[index] = false;
+        let bracketIndex = activeNicknames[index].indexOf('[');
+        if (bracketIndex != -1) {
+            activeNicknames[index] = activeNicknames[index].substring(0, bracketIndex);
+        }
+    }
+    
     return 200;
 }
 

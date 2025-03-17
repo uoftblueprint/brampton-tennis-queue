cce121d1fa5cfd80b1b86158b1d0860d55365360
ron-madan <codewithron@gmail.com>
2025-03-16 23:48:12 -0400

    Renable variables to access public secrets.

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth, Auth } from 'firebase/auth';
 import { getFirestore, Firestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.CONFIG_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_CONFIG_API_KEY,
   authDomain: "brampton-tennis-queue.firebaseapp.com",
   projectId: "brampton-tennis-queue",
   storageBucket: "brampton-tennis-queue.appspot.com",
-  messagingSenderId: process.env.CONFIG_SENDER_ID,
-  appId: process.env.CONFIG_APP_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_CONFIG_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_CONFIG_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);

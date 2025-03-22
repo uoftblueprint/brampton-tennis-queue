d6626aed3e98a1425c3372a5557a4a2ddabf3dab
ron-madan <codewithron@gmail.com>
2025-03-22 16:10:44 -0400

    trying rewrite

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -4,7 +4,8 @@ import { getFirestore, Firestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDJd2dKovgdgS29dqUKG3a0RKZwL_M7C1g",
-  authDomain: "brampton-tennis-queue.firebaseapp.com",
+  //authDomain: "brampton-tennis-queue.firebaseapp.com",
+  authDomain: "brampton-tennis-queue.vercel.app",
   projectId: "brampton-tennis-queue",
   storageBucket: "brampton-tennis-queue.appspot.com",
   messagingSenderId: "578763495949",
